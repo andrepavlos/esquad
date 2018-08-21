18c7f1625e7dea3cbf6d1f2f9c57d4e4d29f5042
andrepavlos <andrepavlos@gmail.com>
2018-08-22 02:11:54 +0500

    update ui theme

--- client/ui/theme.js
+++ client/ui/theme.js
@@ -1,5 +1,14 @@
-export const theme = {
-  colors: {
-    accent: "#ccff00"
-  }
+import color from "tinycolor2";
+
+let theme = {
+  dark: "#333333",
+  accent: "#ffcc00",
+  text: "#eee"
 };
+
+theme = Object.assign(theme, {
+  darkLighten: String(color(theme.dark).lighten(4)),
+  darkDarken: String(color(theme.dark).darken(1))
+});
+
+export { theme };

2db642cc88603a35e17b9142b9614094d2a2eae3
andrepavlos <andrepavlos@gmail.com>
2018-08-22 02:14:10 +0500

    update authentication feature

---
+++ client/features/authentication/authentication.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Button } from "ui";
+
+class Authentication extends React.Component {
+  render() {
+    return (
+      <div>
+        <Button>Sign In</Button>
+      </div>
+    );
+  }
+}
+
+export { Authentication };
--- client/features/index.js
+++ client/features/index.js
@@ -1,2 +1,1 @@
-export * from "./duck";
 export * from "./authentication";

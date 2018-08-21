694a4911bae68f6f73b3cd12b160f0c459ad4852
andrepavlos <andrepavlos@gmail.com>
2018-08-21 18:00:51 +0500

    add root page

---
+++ client/pages/root/page.jsx
@@ -0,0 +1,3 @@
+import React from "react";
+
+export const RootPage = () => <div className="root-page">HOME</div>;
---
+++ client/pages/root/route.js
@@ -0,0 +1,5 @@
+import React from "react";
+import { Route } from "react-router-dom";
+import { RootPage } from "./page";
+
+export const RootRoute = <Route exact path="/" component={RootPage} />;

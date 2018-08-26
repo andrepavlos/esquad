58733add07f6a8c1c917c211dd3ff5ff7ff65b2c
andrepavlos <andrepavlos@gmail.com>
2018-08-26 18:18:41 +0300

    update nav-bar link button

--- client/ui/molecules/nav-bar.jsx
+++ client/ui/molecules/nav-bar.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import styled, { css } from "styled-components";
-import { Link } from "../atoms";
+import styled from "styled-components";
+import { Link, Button } from "../atoms";
 import color from "tinycolor2";
 
 const Container = styled.nav`
@@ -11,25 +11,9 @@ const Container = styled.nav`
   box-shadow: 0 -0.3rem 0.5rem #383838;
 `;
 
-const LinkButton = styled(Link).attrs({
-  activeClassName: "link-button_active"
-})`
-  padding: 1.25rem 2.5rem;
-  font-size: 1.6rem;
-  font-weight: 400;
-  text-transform: uppercase;
-  text-decoration: none;
-  color: ${p => p.theme.text};
-  transition: all 0.15s ease;
-
-  &.${p => p.activeClassName}, &:hover {
-    color: ${p => p.theme.accent};
-    background-color: ${p => String(color(p.theme.darkLighten).lighten(3))};
-    box-shadow: inset 0 -2px 0 ${p => p.theme.accent}, 0 0 3px #333;
-    position: relative;
-    z-index: 1;
-  }
-`;
+const LinkButton = styled(Button.withComponent(Link)).attrs({
+  activeClassName: "active"
+})``;
 
 export const NavBar = styled(props => (
   <Container className={props.className}>

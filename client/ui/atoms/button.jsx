1a887f14cd17e89a13a1e2230a59f08e146cf76a
andrepavlos <andrepavlos@gmail.com>
2018-08-26 18:17:34 +0300

    update ui/button

--- client/ui/atoms/button.jsx
+++ client/ui/atoms/button.jsx
@@ -1,20 +1,88 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 import color from "tinycolor2";
 
-export const Button = styled.button`
+const fontSizeModifier = p => {
+  if (p.small) {
+    return css`
+      font-size: 1.4rem;
+    `;
+  }
+
+  if (p.big) {
+    return css`
+      font-size: 1.8rem;
+    `;
+  }
+
+  return css`
+    font-size: 1.6rem;
+  `;
+};
+
+const paddingModifier = p => {
+  if (p.small)
+    return css`
+      padding: 1rem 2rem;
+    `;
+
+  if (p.big)
+    return css`
+      padding: 1.5rem 3rem;
+    `;
+
+  return css`
+    padding: 1.25rem 2.5rem;
+  `;
+};
+
+const boxRadiusModifier = p => {
+  if (p.rounded) {
+    return css`
+      border-radius: 0.5rem;
+
+      &:hover {
+        box-shadow: none;
+      }
+    `;
+  }
+};
+
+const Button = styled.button`
+  background-color: ${p => p.theme.darkLighten};
   color: ${p => p.theme.text};
+  text-transform: uppercase;
+  width: ${p => (p.wide ? "100%" : "auto")};
   border: none;
   box-shadow: none;
-  padding: 1rem 2rem;
-  display: inline-block;
-  border-radius: 0.5rem;
   cursor: pointer;
-  background-color: transparent;
-  border: 2px solid ${p => String(color(p.theme.darkLighten).lighten(5))};
+  font-weight: 400;
+  text-transform: uppercase;
+  text-decoration: none;
+  color: ${p => p.theme.text};
   transition: all 0.15s ease;
 
-  &:hover {
-    background-color: ${p => String(color(p.theme.darkLighten).lighten(5))};
+  & span {
+    font-weight: 700;
     color: ${p => p.theme.accent};
   }
+
+  &:hover,
+  &.active {
+    color: ${p => p.theme.accent};
+    background-color: ${p => String(color(p.theme.darkLighten).lighten(3))};
+    box-shadow: inset 0 -2px 0 ${p => p.theme.accent}, 0 0 3px #333;
+    position: relative;
+    z-index: 1;
+  }
+
+  & > span {
+    font-weight: 700;
+    color: ${p => p.theme.accent};
+  }
+
+  ${paddingModifier};
+  ${fontSizeModifier};
+  ${boxRadiusModifier};
 `;
+
+export { Button };
